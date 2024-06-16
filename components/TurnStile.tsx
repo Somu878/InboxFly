0170413218c773dc080cb5deafe0e03e79f4601a
somu878 <somumydhili19@gmail.com>
2024-06-16 20:41:09 +0530

    update turnstile

--- components/TurnStile.tsx
+++ components/TurnStile.tsx
@@ -1,14 +1,13 @@
 import { Turnstile } from "@marsidev/react-turnstile";
 import { useState } from "react";
 const key: any = process.env.NEXT_PUBLIC_CLOUDFLARE_TURNSTILE_KEY;
-console.log(key);
 function TurnStile() {
   const [token, setToken] = useState<string | null>(null);
   return (
     <Turnstile
-      //   onSuccess={(token) => {
-      //     setToken(token);
-      //   }}
+      onSuccess={(token) => {
+        setToken(token);
+      }}
       siteKey={key}
     />
   );
