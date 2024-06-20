3474138d67646dd88075e638c7851a1210e408fc
somu878 <somumydhili19@gmail.com>
2024-06-20 21:37:26 +0530

    remove mistral dir

--- app/api/mistral/route.ts
+++
@@ -1,5 +0,0 @@
-import { client } from "../../lib/mistralAi";
-import { redis } from "@/app/lib/redis";
-import { NextRequest, NextResponse } from "next/server";
-
-export async function GET(req: NextRequest) {}
